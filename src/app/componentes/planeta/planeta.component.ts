0a74129d6cc1427e564febbe5b8bc769a4b19aba
Vladoxx <wladox@outlook.com>
2020-08-18 06:03:53 -0300

    Star Wars

---
+++ src/app/componentes/planeta/planeta.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlanetasService } from '../../services/planetas.service';
+
+@Component({
+  selector: 'app-planeta',
+  templateUrl: './planeta.component.html',
+  styleUrls: ['./planeta.component.css']
+})
+export class PlanetaComponent implements OnInit {
+  
+  constructor(private route: ActivatedRoute, private planetaServ: PlanetasService) { }
+  
+  id: number;
+  planeta: any = [];
+
+  ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      
+      this.planetaServ.getPlaneta(this.id)
+      .subscribe(planeta => {
+        this.planeta = planeta;
+        console.log(this.planeta)
+      });
+    });
+  }
+
+}
